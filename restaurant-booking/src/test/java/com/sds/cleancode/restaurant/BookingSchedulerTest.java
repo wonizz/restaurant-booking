0126c905aa72087fedd21937c1021c1c0abc2073
dong_won.lee <dong_won.lee@samsung.com>
2018-09-19 18:53:13 +0900

    최초 커밋

---
+++ restaurant-booking/src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
@@ -0,0 +1,16 @@
+package com.sds.cleancode.restaurant;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class BookingSchedulerTest {
+
+	@Test
+	public void testName() throws Exception {
+		// test
+		fail();
+	}
+}
